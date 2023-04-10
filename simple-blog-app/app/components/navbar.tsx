0fa32a50fb08e22acc856c7aa968cc6873ed2f9e
irfan-arrosid <irfanarrosid4@gmail.com>
2023-04-10 23:30:13 +0700

    Creating social media icons to the navbar component

--- simple-blog-app/app/components/navbar.tsx
+++ simple-blog-app/app/components/navbar.tsx
@@ -1,11 +1,25 @@
 // import React from 'react'
 import Link from 'next/link'
+import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa'
 
 export default function Navbar() {
   return (
     <nav className='bg-slate-800 p-4 sticky top-0 drop-shadow-xl z-10'>
         <div className='prose prose-xl mx-auto flex justify-between flex-col sm:flex-row'>
+          <h1 className='text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0'>
             <Link href='/' className='text-white/90 no-underline hover:text-white'>Irfan Arrosid</Link>
+          </h1>
+          <div className='flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl'>
+              <Link className='text-white/90 hover:text-white' href='https://www.linkedin.com/in/atdjli-irfan-arrosid-6a8467191/'>
+                  <FaLinkedin />
+              </Link>
+              <Link className='text-white/90 hover:text-white' href='https://github.com/irfan-arrosid'>
+                  <FaGithub />
+              </Link>
+              <Link className='text-white/90 hover:text-white' href='https://twitter.com/irfan_arrosid'>
+                  <FaTwitter />
+              </Link>
+          </div>
         </div>
     </nav>
   )
